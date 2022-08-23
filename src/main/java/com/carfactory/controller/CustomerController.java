95ee0042a6ff59acf7ffbe35e09f141163b34c11
ibrahimHesham <ibrahim.hesham.cs@gmail.com>
2022-08-23 13:37:58 +0200

    Adding customer update, delete, get customer by id, get customer orders

--- src/main/java/com/carfactory/controller/CustomerController.java
+++ src/main/java/com/carfactory/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package com.carfactory.controller;
 
 import com.carfactory.model.Customer;
+import com.carfactory.model.Order;
+import com.carfactory.model.Product;
 import com.carfactory.model.ProductOption;
 import com.carfactory.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +21,41 @@ public class CustomerController {
     }
 
     @GetMapping
-    public List<Customer> getCustomer(){
+    public List<Customer> getCustomers(){
         return customerService.getCustomers();
     }
 
     @PostMapping
     public Customer addCustomer(@RequestBody Customer customer){
         return customerService.addCustomer(customer);
+    }
+
+    @DeleteMapping(
+            path = "{customerId}"
+    )
+    public void deleteCustomer(@PathVariable("customerId") Long id){
+        customerService.deleteCustomer(id);
+    }
+
+    @PutMapping(
+            path = "{customerId}"
+    )
+    public Customer updateCustomer(@RequestBody Customer customer, @PathVariable("customerId") Long id){
+        return customerService.updateCustomer(customer, id);
+    }
+
+    @GetMapping(
+            path = "{customerId}"
+    )
+    public Customer getCustomer(@PathVariable("customerId") Long id){
+        return customerService.getCustomerById(id);
+    }
+
+    @GetMapping(
+            path = "{customerId}/orders"
+    )
+    public List<Order> getProductOptionsByProductId(@PathVariable("customerId") Long id){
+        return customerService.getOrdersByCustomerId(id);
     }
 
 
--- src/main/java/com/carfactory/service/CustomerService.java
+++ src/main/java/com/carfactory/service/CustomerService.java
@@ -1,21 +1,28 @@
 package com.carfactory.service;
 
 import com.carfactory.model.Customer;
+import com.carfactory.model.Order;
+import com.carfactory.model.Product;
 import com.carfactory.model.ProductOption;
 import com.carfactory.repository.CustomerRepository;
+import com.carfactory.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
 
     private CustomerRepository customerRepository;
 
+    private OrderRepository orderRepository;
+
     @Autowired
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, OrderRepository orderRepository) {
         this.customerRepository = customerRepository;
+        this.orderRepository = orderRepository;
     }
 
     public List<Customer> getCustomers(){
@@ -25,5 +32,32 @@ public class CustomerService {
 
     public Customer addCustomer(Customer customer){
         return customerRepository.save(customer);
+    }
+
+    public void deleteCustomer(Long id){
+        if(!customerRepository.existsById(id)){
+            throw new IllegalStateException("Customer Not Found");
+        }
+        customerRepository.deleteById(id);
+    }
+
+    public Customer updateCustomer(Customer reqCustomer,Long id){
+        if(!customerRepository.existsById(id)){
+            throw new IllegalStateException("Customer Not Found");
+        }
+
+        return customerRepository.save(reqCustomer);
+    }
+
+    public Customer getCustomerById(Long id){
+        Optional<Customer> customerOptional = customerRepository.findById(id);
+        if(!customerOptional.isPresent()){
+            throw new IllegalStateException("Customer Not Found");
+        }
+        return customerOptional.get();
+    }
+
+    public List<Order> getOrdersByCustomerId(Long id){
+        return orderRepository.findByCustomer_Id(id);
     }
 }
